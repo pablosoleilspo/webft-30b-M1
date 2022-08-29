a51b7c8ca0e3b97f5139f09a3f2acc89ec8159b1
pablo vasquez <pablosoleilspo@gmail.com>
2022-08-29 15:55:53 -0500

    first commit

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,13 +1,24 @@
 'use strict'
 
-function BinarioADecimal(num) {
+function BinarioADecimal(num) { 
   // tu codigo aca
-
+  var decimal=0;
+for(let i=0; i<num.length; i++)
+{
+decimal= decimal + (num[i] * 2 ** (num.length - 1 - i));
+}
+return decimal;
 }
 
-function DecimalABinario(num) {
+function DecimalABinario(num) { 
   // tu codigo aca
-
+  var array= [];
+while(num>0)
+{
+  array.unshift(num%2)
+  num=Math.floor(num/2)
+}
+return array.join('');
 }
 
 
